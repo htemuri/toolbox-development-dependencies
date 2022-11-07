f4d45e793702b6e4194d8624b68228133743fcd8
htemuri <htemuri@gmail.com>
2022-11-06 19:02:14 -0500

    shell script for tools

---
+++ install_tools.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# REPOS
+
+# vscode repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+
+# Update dnf
+dnf check-update
+dnf upgrade
+
+sudo dnf install code clang
